b923feeefd422e0db6190c961bec2a8356d0153f
Didier Matos <d.matostorres@gmail.com>
2019-03-06 07:48:31 -0500

    Se agregaron las variables para enlazar con el id los componentes del layour

--- app/src/main/java/com/nohochdevelopers/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nohochdevelopers/interactivestory/ui/StoryActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nohochdevelopers.interactivestory.R;
@@ -15,11 +16,21 @@ public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
     private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
+
 
         Intent intent = getIntent();
         String name = intent.getStringExtra(getString(R.string.key_name));
