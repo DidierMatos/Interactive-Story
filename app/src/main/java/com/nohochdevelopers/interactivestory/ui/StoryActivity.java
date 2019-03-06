ad68b897827d69fda57b07bd14e9aac338fa237d
Didier Matos <d.matostorres@gmail.com>
2019-03-06 07:44:37 -0500

    Se agregó el metodo loadPage

--- app/src/main/java/com/nohochdevelopers/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nohochdevelopers/interactivestory/ui/StoryActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.nohochdevelopers.interactivestory.R;
+import com.nohochdevelopers.interactivestory.model.Page;
+import com.nohochdevelopers.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+    private Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,15 @@ public class StoryActivity extends AppCompatActivity {
         Log.d(TAG, name);
         //Toast.makeText(this,name, Toast.LENGTH_SHORT).show();
 
+        story = new Story();
+        loadPage(0);
 
+
+
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
 
 
     }
